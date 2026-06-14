c9ed1c812562031cfcb68a7a83d07b17492e5fca
stonea1068 <stonea1068@student.faytechcc.edu>
2026-06-14 14:47:56 -0400

    Add files via upload

---
+++ P1LAB1_StoneAshley.py
@@ -0,0 +1,12 @@
+# Ashley Stone
+# 06/14/2026
+# P1LAB1
+
+# Ask for first name
+first_name = input("Enter your first name: ")
+
+# Ask for last name
+last_name = input("Enter your last name: ")
+
+# Display greeting
+print("Hello,", first_name, last_name + "!", "Welcome to CTI-110")
